99d4ffde41a5ac5f1d7bc2cb96e503c0f9dca3ab
poseidon1236 <546534112@qq.com>
2017-04-21 14:05:26 +0800

    Add the tracking camera

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    private Camera m_camera = null;
+
+	// Use this for initialization
+	void Start () {
+        m_camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_camera.transform.position = gameObject.transform.position - 
+            gameObject.transform.forward * 5 + gameObject.transform.up * 3;
+        m_camera.transform.LookAt(gameObject.transform.position, Vector3.up);
+	}
+}
