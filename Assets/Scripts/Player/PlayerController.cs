9b74d468dd9abdec7883f10090ba5139437f007e
poseidon1236 <546534112@qq.com>
2017-05-12 10:07:31 +0800

    Add new player, including model, animator and scripts.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private PlayerState m_Character; // A reference to the ThirdPersonCharacter on the object
+    private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+    private Vector3 m_CamForward;             // The current forward direction of the camera
+    private Vector3 m_Move;
+    private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+
+    private void Start()
+    {
+        // get the transform of the main camera
+        if (Camera.main != null)
+        {
+            m_Cam = Camera.main.transform;
+        }
+        else
+        {
+            Debug.LogWarning(
+                "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+            // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+        }
+
+        // get the third person character ( this should never be null due to require component )
+        m_Character = GetComponent<PlayerState>();
+    }
+
+
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            m_Jump = Input.GetKeyDown(KeyCode.W);
+        }
+    }
+
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        // read inputs
+        bool crouch = Input.GetKey(KeyCode.S);
+
+        // calculate move direction to pass to character
+        m_Move = Vector3.forward;
+
+        // pass all parameters to the character control script
+        m_Character.Move(m_Move, crouch, m_Jump);
+        m_Jump = false;
+    }
 }
